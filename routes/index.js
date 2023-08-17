c19fb4a048e71f60b2eb17fbff757cfc2d5076b8
wellington90 <126090580+wellington90@users.noreply.github.com>
2023-08-17 16:11:57 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express-2023' });
+  res.render('index', { title: 'Express-18/08/2023' });
 });
 
 module.exports = router;
